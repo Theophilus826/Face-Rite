8c76246a34eff6ec0a73523fa96fb107d4528afc
Theophilus <theophilustelecom@gmail.com>
2026-07-18 01:28:30 +0100

    update

--- src/bubble/Game.js
+++ src/bubble/Game.js
@@ -1,11 +1,4 @@
-import {
-  Engine,
-  Scene,
-  Vector3,
-  MeshBuilder,
-  Color3,
-  HemisphericLight,
-} from "@babylonjs/core";
+import * as BABYLON from "@babylonjs/core";
 import * as GUI from "@babylonjs/gui";
 import { Board } from "./Board.js";
 import { Shooter } from "./Shooter.js";
